c9cb2d828b2d2b9b63c19085809f6254c3aa4e8d
evaevangelisti <eva.evangelisti03@gmail.com>
2025-06-15 17:28:24 +0200

    refactor: general improvements in run.sh

--- run.sh
+++ run.sh
@@ -15,33 +15,25 @@ module load profile/lifesc
 module load gromacs/2021.3--intel-oneapi-mpi--2021.4.0--intel--2021.4.0-cuda-11.5.0
 
 INPUT_DIR="./data/raw/microalgae"
-CO2_PDB="./data/raw/co2.pdb"
-
 PROCESSED_DIR="./data/processed"
 RESULTS_DIR="./results"
 
 main() {
+  rm -rf "$PROCESSED_DIR" "$RESULTS_DIR" logs
   mkdir -p "$PROCESSED_DIR" "$RESULTS_DIR" logs
 
   for pdb in "$INPUT_DIR"/*.pdb; do
-    echo "$pdb: starting cleaning"
-
-    cleaned_pdb=$(./scripts/clear_pdb.sh -p "$pdb" -o "$PROCESSED_DIR") || {
+    ./scripts/clear_pdb.sh -p "$pdb" -o "$PROCESSED_DIR" || {
       echo "$pdb: cleaning failed" >&2
       continue
     }
 
-    echo "$pdb: starting simulation"
-
-    ./scripts/simulate.sh -p "$cleaned_pdb" -c "$CO2_PDB" -o "$RESULTS_DIR" || {
+    ./scripts/simulate.sh -p "$PROCESSED_DIR/$(basename "$pdb" .pdb)_clean.pdb" -o "$RESULTS_DIR" || {
       echo "$pdb: simulation failed" >&2
       continue
     }
-
-    echo "$pdb: simulation completed successfully"
   done
-
-  conda deactivate
 }
 
 main "$@"
+conda deactivate
